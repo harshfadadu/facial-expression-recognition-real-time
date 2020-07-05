ce653946009e72bcdc24782aefcc348d7cff06e7
Harsh Fadadu <37452761+harshfadadu@users.noreply.github.com>
2020-07-05 15:58:33 +0530

    Update train_gender_classifier.py

--- src/train_gender_classifier.py
+++ src/train_gender_classifier.py
@@ -1,11 +1,3 @@
-"""
-File: train_gender_classifier.py
-Author: Octavio Arriaga
-Email: arriaga.camargo@gmail.com
-Github: https://github.com/oarriaga
-Description: Train gender classification model
-"""
-
 from keras.callbacks import CSVLogger, ModelCheckpoint, EarlyStopping
 from keras.callbacks import ReduceLROnPlateau
 from utils.datasets import DataManager
